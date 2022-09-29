d2f2d0cc0792c03752f07a430cfbc4169bf83181
nhut.truong <tlmnhut2207@gmail.com>
2022-09-30 00:57:07 +0200

    rename main script

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for brain_area in "vTC" "PPA" "FFA"
+for brain_area in "vTC" "PPA"
 do
     for filter_size in {24..56..4}
     do
         for use_pruning in 0 1
         do
             echo "$brain_area $filter_size $use_pruning"
-            python saliency_map.py --brain_area $brain_area --filter_size $filter_size --use_pruning $use_pruning
+            python compute_perturbation.py --brain_area $brain_area --filter_size $filter_size --use_pruning $use_pruning
         done
     done
 done
